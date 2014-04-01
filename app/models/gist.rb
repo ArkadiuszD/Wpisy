d70c6d2198e0c4a51d6a17ef0f33624e6e34bc44
ArkadiuszD <aarkadiusz.dabrowski@gmail.com>
2014-04-02 00:54:01 +0200

    pierwsza wersja

---
+++ app/models/gist.rb
@@ -0,0 +1,16 @@
+class Gist < ActiveRecord::Base
+
+self.per_page = 8
+
+def self.search(search)
+	if search
+		find(:all, :conditions => ['description LIKE ?',"%#{search}%"])
+	else
+		find(:all)
+	end
+end	
+
+
+end
+
+
